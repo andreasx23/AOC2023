41d72df2d45be682264070ca09ab37f3ea6622b6
andreasx23 <49310598+andreasx23@users.noreply.github.com>
2023-12-18 17:31:55 +0100

    Solved day 14 part 2

--- AOC2023/Day14/Day14Part2.cs
+++ AOC2023/Day14/Day14Part2.cs
@@ -18,30 +18,49 @@ namespace AOC2023.Day14
             RIGHT
         }
 
-        private static readonly bool _useTestData = true;
+        private static readonly bool _useTestData = false;
         private static readonly string _className = "Day14";
         private List<List<char>> _data = new();
+        private Dictionary<string, List<List<char>>> _cache = new();
 
         public long Solve(Stopwatch watch)
         {
             long sum = 0;
 
-            // Currently not done with this one yet!
-
-            var cycles = _useTestData ? 1 : 1000000000;
+            var cycles = 1000000000;
+            bool firstCacheHit = true;
             for (int i = 0; i < cycles; i++)
             {
+                var key = GetKey();
+                if (_cache.TryGetValue(key, out var value))
+                {
+                    if (firstCacheHit)
+                    {
+                        var cyclesLeft = cycles % _cache.Count;
+                        i = cycles - cyclesLeft;
+                        firstCacheHit = false;
+                    }
+
+                    sum = Math.Max(sum, Sum());
+
+                    _data = value;
+                    continue;
+                }
+
                 Tilt(Direction.UP);
                 Tilt(Direction.LEFT);
                 Tilt(Direction.DOWN);
                 Tilt(Direction.RIGHT);
+
+                _cache[key] = Clone();
             }
 
-            foreach (var item in _data)
-            {
-                Console.WriteLine(string.Join("", item));
-            }
+            return sum;
+        }
 
+        private long Sum()
+        {
+            var sum = 0L;
             for (int i = 0; i < _data.Count; i++)
             {
                 for (int j = 0; j < _data[i].Count; j++)
@@ -56,7 +75,55 @@ namespace AOC2023.Day14
             return sum;
         }
 
+        private string GetKey()
+        {
+            StringBuilder sb = new();
+            foreach (var row in _data)
+            {
+                foreach (var c in row)
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private List<List<char>> Clone()
+        {
+            return _data.Select(row => row.ToList()).ToList();
+        }
+
+        private void Print()
+        {
+            foreach (var item in _data)
+            {
+                Console.WriteLine(string.Join("", item));
+            }
+
+            Console.WriteLine();
+        }
+
         private void Tilt(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    TiltUp();
+                    break;
+                case Direction.DOWN:
+                    TiltDown();
+                    break;
+                case Direction.LEFT:
+                    TiltLeft();
+                    break;
+                case Direction.RIGHT:
+                    TiltRight();
+                    break;
+            }
+        }
+
+        private void TiltUp()
         {
             for (int i = 1; i < _data.Count; i++)
             {
@@ -64,96 +131,116 @@ namespace AOC2023.Day14
                 {
                     if (_data[i][j] == 'O')
                     {
-                        switch (direction)
+                        var k = i;
+                        while (k - 1 >= 0)
                         {
-                            case Direction.UP:
-                                {
-                                    var k = i;
-                                    while (k - 1 >= 0)
-                                    {
-                                        if (_data[k - 1][j] == '.')
-                                        {
-                                            k--;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-                                    }
-
-                                    if (i != k)
-                                    {
-                                        _data[k][j] = 'O';
-                                        _data[i][j] = '.';
-                                    }
-                                }
+                            if (_data[k - 1][j] == '.')
+                            {
+                                k--;
+                            }
+                            else
+                            {
                                 break;
-                            case Direction.DOWN:
-                                {
-                                    var k = i;
-                                    while (k + 1 < _data.Count)
-                                    {
-                                        if (_data[k + 1][j] == '.')
-                                        {
-                                            k++;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-                                    }
+                            }
+                        }
 
-                                    if (i != k)
-                                    {
-                                        _data[k][j] = 'O';
-                                        _data[i][j] = '.';
-                                    }
-                                }
-                                break;
-                            case Direction.LEFT:
-                                {
-                                    var k = j;
-                                    while (k - 1 >= 0)
-                                    {
-                                        if (_data[i][k - 1] == '.')
-                                        {
-                                            k--;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-                                    }
+                        if (i != k)
+                        {
+                            _data[k][j] = 'O';
+                            _data[i][j] = '.';
+                        }
+                    }
+                }
+            }
+        }
 
-                                    if (j != k)
-                                    {
-                                        _data[i][k] = 'O';
-                                        _data[i][j] = '.';
-                                    }
-                                }
+        private void TiltDown()
+        {
+            for (int i = _data.Count - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < _data[i].Count; j++)
+                {
+                    if (_data[i][j] == 'O')
+                    {
+                        var k = i;
+                        while (k + 1 < _data.Count)
+                        {
+                            if (_data[k + 1][j] == '.')
+                            {
+                                k++;
+                            }
+                            else
+                            {
                                 break;
-                            case Direction.RIGHT:
-                                {
-                                    var k = j;
-                                    while (k + 1 < _data.First().Count)
-                                    {
-                                        if (_data[i][k + 1] == '.')
-                                        {
-                                            k++;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-                                    }
+                            }
+                        }
 
-                                    if (j != k)
-                                    {
-                                        _data[i][k] = 'O';
-                                        _data[i][j] = '.';
-                                    }
-                                }
+                        if (i != k)
+                        {
+                            _data[k][j] = 'O';
+                            _data[i][j] = '.';
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TiltLeft()
+        {
+            for (int i = 0; i < _data.Count; i++)
+            {
+                for (int j = 0; j < _data[i].Count; j++)
+                {
+                    if (_data[i][j] == 'O')
+                    {
+                        var k = j;
+                        while (k - 1 >= 0)
+                        {
+                            if (_data[i][k - 1] == '.')
+                            {
+                                k--;
+                            }
+                            else
+                            {
                                 break;
+                            }
+                        }
+
+                        if (j != k)
+                        {
+                            _data[i][k] = 'O';
+                            _data[i][j] = '.';
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TiltRight()
+        {
+            for (int i = 0; i < _data.Count; i++)
+            {
+                for (int j = _data[i].Count - 1; j >= 0; j--)
+                {
+                    if (_data[i][j] == 'O')
+                    {
+                        var k = j;
+                        while (k + 1 < _data.First().Count)
+                        {
+                            if (_data[i][k + 1] == '.')
+                            {
+                                k++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (j != k)
+                        {
+                            _data[i][k] = 'O';
+                            _data[i][j] = '.';
                         }
                     }
                 }
