c295426f56559bfa7a9d9bf9471fe23093245edf
andreasx23 <49310598+andreasx23@users.noreply.github.com>
2023-12-19 19:57:21 +0100

    Temp 2

--- AOC2023/Day19/Day19Part2.cs
+++ AOC2023/Day19/Day19Part2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -48,25 +49,34 @@ namespace AOC2023.Day19
                 }
                 else
                 {
-                    Min = 0;
+                    Min = 1;
                     Max = Amount;
                 }
             }
 
-            public bool CheckIfValid(int partAmount)
+            public bool CheckIfValid(int min, int max)
             {
-                return Min <= partAmount && partAmount <= Max;
+                //return Min <= min && max <= Max;
+                return min <= Min && Max <= max;
             }
         }
 
         class Part
         {
             public string Name { get; set; }
-            public int ExtremelyGoodLooking { get; set; } // x
-            public int Musical { get; set; } // m
-            public int Aerodynamic { get; set; } // a
-            public int Shiny { get; set; } // s
-            public int Score => ExtremelyGoodLooking + Musical + Aerodynamic + Shiny;
+            public int ExtremelyGoodLookingMin { get; } = 1; // x
+            public int ExtremelyGoodLookingMax { get; set; } // x
+            public int MusicalMin { get; } = 1; // m
+            public int MusicalMax { get; set; } // m
+            public int AerodynamicMin { get; } = 1; // a
+            public int AerodynamicMax { get; set; } // a
+            public int ShinyMin { get; } = 1; // s
+            public int ShinyMax { get; set; } // s
+
+            public long Score => (ExtremelyGoodLookingMax - ExtremelyGoodLookingMin + 1)
+                                 * (MusicalMax - MusicalMin + 1)
+                                 * (AerodynamicMax - AerodynamicMin + 1)
+                                 * (ShinyMax - ShinyMin + 1);
         }
 
         private static readonly bool _useTestData = true;
@@ -80,44 +90,134 @@ namespace AOC2023.Day19
         {
             long sum = 0;
 
-            object @lock = new();
-            long count = 0;
-            long batchMax = 10000;
-            long total = 4000L * 4000L * 4000L * 4000L;
-            for (int x = 1; x <= 4000; x++)
+            sum = Bfs();
+
+            return sum;
+        }
+
+        // 788846272
+        // 1361750208
+        // 2073604890
+        // 1410324064
+        // 376801888
+        // 167409079868000
+        private long Bfs()
+        {
+            Queue<(string workFlowName, int xl, int xh, int ml, int mh, int al, int ah, int sl, int sh)> queue = new();
+            queue.Enqueue((START_WORKFLOW, 1, 4000, 1, 4000, 1, 4000, 1, 4000));
+
+            long sum = 0;
+            while (queue.Count > 0)
             {
-                for (int m = 1; m <= 4000; m++)
+                var (workFlowName, xl, xh, ml, mh, al, ah, sl, sh) = queue.Dequeue();
+
+                if (xl > xh || ml > mh || al > ah || sl > sh)
                 {
-                    for (int a = 1; a <= 4000; a++)
+                    continue;
+                }
+                else if (workFlowName == "A")
+                {
+                    sum += (xh - xl + 1)
+                           * (mh - ml + 1)
+                           * (ah - al + 1)
+                           * (sh - sl + 1);
+                    continue;
+                }
+                else if (workFlowName == "R")
+                {
+                    continue;
+                }
+                else
+                {
+                    var workflow = _workflows[workFlowName];
+                    foreach (var rule in workflow.Rules)
                     {
-                        for (int s = 1; s <= 4000; s++)
+                        if (rule.IsSpecialRule)
                         {
-                            count++;
-
-                            _parts.Add(new Part()
+                            queue.Enqueue((rule.SendTo, xl, xh, ml, mh, al, ah, sl, sh));
+                        }
+                        else
+                        {
+                            switch (rule.Name)
                             {
-                                ExtremelyGoodLooking = x,
-                                Musical = m,
-                                Aerodynamic = a,
-                                Shiny = s
-                            });
-
-                            if (_parts.Count == batchMax)
-                            {
-                                Console.WriteLine($"[{watch.Elapsed}] Processing batch of: {batchMax} - Done with: {count} / {total}");
-
-                                Parallel.ForEach(_parts, part =>
-                                {
-                                    var localSum = VerifyPart(part);
-                                    lock (@lock)
+                                case "x":
                                     {
-                                        sum += localSum;
+                                        var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, xl, xh);
+                                        queue.Enqueue((rule.SendTo, min, max, ml, mh, al, ah, sl, sh));
+
+                                        (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, xl, xh);
+                                        xl = min;
+                                        xh = max;
                                     }
-                                });
+                                    //if (rule.CheckIfValid(xl, xh))
+                                    //{
+                                    //    var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, xl, xh);
+                                    //    queue.Enqueue((rule.SendTo, min, max, ml, mh, al, ah, sl, sh));
 
-                                //Console.WriteLine($"[{watch.Elapsed}] Current sum: {sum}");
+                                    //    (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, xl, xh);
+                                    //    xl = min;
+                                    //    xh = max;
+                                    //}
+                                    break;
+                                case "m":
+                                    //if (rule.CheckIfValid(ml, mh))
+                                    //{
+                                    //    var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, ml, mh);
+                                    //    queue.Enqueue((rule.SendTo, xl, xh, min, max, al, ah, sl, sh));
 
-                                _parts.Clear();
+                                    //    (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, ml, mh);
+                                    //    ml = min;
+                                    //    mh = max;
+                                    //}
+                                    {
+                                        var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, ml, mh);
+                                        queue.Enqueue((rule.SendTo, xl, xh, min, max, al, ah, sl, sh));
+
+                                        (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, ml, mh);
+                                        ml = min;
+                                        mh = max;
+                                    }
+                                    break;
+                                case "a":
+                                    //if (rule.CheckIfValid(al, ah))
+                                    //{
+                                    //    var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, al, ah);
+                                    //    queue.Enqueue((rule.SendTo, xl, xh, ml, mh, min, max, sl, sh));
+
+                                    //    (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, al, ah);
+                                    //    al = min;
+                                    //    ah = max;
+                                    //}
+                                    {
+                                        var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, al, ah);
+                                        queue.Enqueue((rule.SendTo, xl, xh, ml, mh, min, max, sl, sh));
+
+                                        (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, al, ah);
+                                        al = min;
+                                        ah = max;
+                                    }
+                                    break;
+                                case "s":
+                                    //if (rule.CheckIfValid(sl, sh))
+                                    //{
+                                    //    var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, sl, sh);
+                                    //    queue.Enqueue((rule.SendTo, xl, xh, ml, mh, sl, sh, min, max));
+
+                                    //    (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, sl, sh);
+                                    //    sl = min;
+                                    //    sh = max;
+                                    //}
+                                    {
+                                        var (min, max) = CalculateMinMax(rule.Operator, rule.Amount, sl, sh);
+                                        queue.Enqueue((rule.SendTo, xl, xh, ml, mh, sl, sh, min, max));
+
+                                        (min, max) = CalculateMinMax(rule.Operator == ">" ? "<=" : ">=", rule.Amount, sl, sh);
+                                        sl = min;
+                                        sh = max;
+                                    }
+                                    break;
+                                default:
+                                    throw new Exception();
                             }
                         }
                     }
@@ -127,68 +227,25 @@ namespace AOC2023.Day19
             return sum;
         }
 
-        private long VerifyPart(Part part)
+        private (int min, int max) CalculateMinMax(string @operator, int amount, int min, int max)
         {
-            var workflow = _workflows[START_WORKFLOW];
-            bool isDone = false;
-            while (!isDone)
+            switch (@operator)
             {
-                bool isValid = true;
-                string sendToWorkFlow = "";
-                foreach (var rule in workflow.Rules)
-                {
-                    isValid = true;
-
-                    if (rule.IsSpecialRule)
-                    {
-                        if (rule.IsRejected)
-                        {
-                            isValid = false;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        isValid = rule.Name switch
-                        {
-                            "x" => rule.CheckIfValid(part.ExtremelyGoodLooking),
-                            "m" => rule.CheckIfValid(part.Musical),
-                            "a" => rule.CheckIfValid(part.Aerodynamic),
-                            "s" => rule.CheckIfValid(part.Shiny),
-                            _ => throw new Exception("Invalid"),
-                        };
-                    }
-
-                    if (isValid)
-                    {
-                        sendToWorkFlow = rule.SendTo;
-                        break;
-                    }
-                }
-
-                if (string.IsNullOrEmpty(sendToWorkFlow) || sendToWorkFlow == "R")
-                {
-                    isValid = false;
-                }
-
-                if (isValid)
-                {
-                    if (sendToWorkFlow == "A")
-                    {
-                        return part.Score;
-                    }
-                    else
-                    {
-                        workflow = _workflows[sendToWorkFlow];
-                    }
-                }
-                else
-                {
+                case ">":
+                    min = Math.Max(min, amount + 1);
                     break;
-                }
+                case "<":
+                    max = Math.Min(max, amount - 1);
+                    break;
+                case ">=":
+                    min = Math.Max(min, amount);
+                    break;
+                case "<=":
+                    max = Math.Min(max, amount);
+                    break;
             }
 
-            return 0;
+            return (min, max);
         }
 
         public void Result()
