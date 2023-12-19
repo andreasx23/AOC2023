a86e54773d41f4431dbff2dc2e4bfddde3da20d8
andreasx23 <49310598+andreasx23@users.noreply.github.com>
2023-12-19 20:38:26 +0100

    Removed unused code

--- AOC2023/Day19/Day19Part2.cs
+++ AOC2023/Day19/Day19Part2.cs
@@ -27,57 +27,7 @@ namespace AOC2023.Day19
             public int Amount { get; set; } // 10
             public string Name { get; set; } // x
             public string SendTo { get; set; } // one
-
-            public int Min { get; set; }
-            public int Max { get; set; }
-
             public bool IsSpecialRule => string.IsNullOrEmpty(Operator);
-            public bool IsAccepted => IsSpecialRule && Name == "A"; // A = Accepted, R = Rejected
-            public bool IsRejected => IsSpecialRule && Name == "R"; // A = Accepted, R = Rejected
-            public bool IsGreaterThanOperator => !string.IsNullOrEmpty(Operator) && Operator == ">";
-
-            public void SetupRange()
-            {
-                if (IsSpecialRule)
-                {
-                    return;
-                }
-
-                if (Operator == ">")
-                {
-                    Min = Amount;
-                    Max = 4000;
-                }
-                else
-                {
-                    Min = 1;
-                    Max = Amount;
-                }
-            }
-
-            public bool CheckIfValid(int min, int max)
-            {
-                //return Min <= min && max <= Max;
-                return min <= Min && Max <= max;
-            }
-        }
-
-        class Part
-        {
-            public string Name { get; set; }
-            public int ExtremelyGoodLookingMin { get; } = 1; // x
-            public int ExtremelyGoodLookingMax { get; set; } // x
-            public int MusicalMin { get; } = 1; // m
-            public int MusicalMax { get; set; } // m
-            public int AerodynamicMin { get; } = 1; // a
-            public int AerodynamicMax { get; set; } // a
-            public int ShinyMin { get; } = 1; // s
-            public int ShinyMax { get; set; } // s
-
-            public long Score => (ExtremelyGoodLookingMax - ExtremelyGoodLookingMin + 1)
-                                 * (MusicalMax - MusicalMin + 1)
-                                 * (AerodynamicMax - AerodynamicMin + 1)
-                                 * (ShinyMax - ShinyMin + 1);
         }
 
         private static readonly bool _useTestData = true;
@@ -90,8 +40,9 @@ namespace AOC2023.Day19
         {
             long sum = 0;
 
-            sum = Test();
-            Console.WriteLine(Bfs());
+            Console.WriteLine("167409079868000");
+            sum = Bfs();
+            Console.WriteLine(sum);
 
             return sum;
         }
@@ -106,123 +57,9 @@ namespace AOC2023.Day19
         // 1039244853168000
         // 128299639868000
         // 167409079868000
-        private long Test()
-        {
-            Queue<(string, int, int, int, int, int, int, int, int)> queue = new();
-            queue.Enqueue(("in", 1, 4000, 1, 4000, 1, 4000, 1, 4000));
-
-            long ans = 0;
-            while (queue.Count > 0)
-            {
-                var current = queue.Dequeue();
-                var key = current.Item1;
-                var xl = current.Item2;
-                var xh = current.Item3;
-                var ml = current.Item4;
-                var mh = current.Item5;
-                var al = current.Item6;
-                var ah = current.Item7;
-                var sl = current.Item8;
-                var sh = current.Item9;
-
-                if (xl > xh || ml > mh || al > ah || sl > sh)
-                {
-                    continue;
-                }
-                else if (key == "A")
-                {
-                    long s1 = xh - xl + 1;
-                    long s2 = mh - ml + 1;
-                    long s3 = ah - al + 1;
-                    long s4 = sh - sl + 1;
-                    ans += s1 * s2 * s3 * s4;
-                    continue;
-                }
-                else if (key == "R")
-                {
-                    continue;
-                }
-                else
-                {
-                    var workflow = _workflows[key];
-                    foreach (var rule in workflow.FullName[(workflow.Name.Length + 1)..^1].Split(','))
-                    {
-                        var name = rule;
-                        if (rule.Contains(':'))
-                        {
-                            var parts = rule.Split(':');
-                            name = parts.Last();
-                            var cond = parts[0];
-                            var var = cond[0].ToString();
-                            var op = cond[1].ToString();
-                            var n = int.Parse(cond[2..]);
-
-                            queue.Enqueue((name, xl, xh, ml, mh, al, ah, sl, sh));
-                            (xl, xh, ml, mh, al, ah, sl, sh) = NewRanges(var, op == ">" ? "<=" : ">=", n, xl, xh, ml, mh, al, ah, sl, sh);
-                        }
-                        else
-                        {
-                            queue.Enqueue((name, xl, xh, ml, mh, al, ah, sl, sh));
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return ans;
-        }
-
-        public static (int, int, int, int, int, int, int, int) NewRanges(string var, string op, int n, int xl, int xh, int ml, int mh, int al, int ah, int sl, int sh)
-        {
-            if (var == "x")
-            {
-                (xl, xh) = NewRange(op, n, xl, xh);
-            }
-            else if (var == "m")
-            {
-                (ml, mh) = NewRange(op, n, ml, mh);
-            }
-            else if (var == "a")
-            {
-                (al, ah) = NewRange(op, n, al, ah);
-            }
-            else if (var == "s")
-            {
-                (sl, sh) = NewRange(op, n, sl, sh);
-            }
-
-            return (xl, xh, ml, mh, al, ah, sl, sh);
-        }
-
-        public static (int, int) NewRange(string op, int n, int lo, int hi)
-        {
-            if (op == ">")
-            {
-                lo = Math.Max(lo, n + 1);
-            }
-            else if (op == "<")
-            {
-                hi = Math.Min(hi, n - 1);
-            }
-            else if (op == ">=")
-            {
-                lo = Math.Max(lo, n);
-            }
-            else if (op == "<=")
-            {
-                hi = Math.Min(hi, n);
-            }
-            else
-            {
-                throw new ArgumentException("Invalid operation");
-            }
-
-            return (lo, hi);
-        }
-
         private long Bfs()
         {
-            Queue<(string workFlowName, int xl, int xh, int ml, int mh, int al, int ah, int sl, int sh)> queue = new();
+            Queue<(string workFlowName, long xl, long xh, long ml, long mh, long al, long ah, long sl, long sh)> queue = new();
             queue.Enqueue((START_WORKFLOW, 1, 4000, 1, 4000, 1, 4000, 1, 4000));
 
             long sum = 0;
@@ -312,7 +149,7 @@ namespace AOC2023.Day19
             return sum;
         }
 
-        private (int min, int max) CalculateMinMax(string @operator, int amount, int low, int high)
+        private (long min, long max) CalculateMinMax(string @operator, int amount, long low, long high)
         {
             switch (@operator)
             {
@@ -414,8 +251,6 @@ namespace AOC2023.Day19
                             rule.Name = ruleStr;
                             rule.SendTo = ruleStr;
                         }
-
-                        rule.SetupRange();
 
                         workflow.Rules.Add(rule);
                     }
