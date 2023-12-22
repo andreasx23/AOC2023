2c64480410e33f41cbbc1d9b2e262d431b2d3b0f
ACA <andreas.cramer.antonsen@gmail.com>
2023-12-22 10:39:05 +0100

    Day 21 part 2 not yet solved

--- AOC2023/Day21/Day21Part2.cs
+++ AOC2023/Day21/Day21Part2.cs
@@ -11,7 +11,7 @@ namespace AOC2023.Day21
 {
     public class Day21Part2
     {
-        private static readonly bool _useTestData = false;
+        private static readonly bool _useTestData = true;
         private static readonly string _className = "Day21";
         private List<List<char>> _data = new();
 
@@ -19,7 +19,7 @@ namespace AOC2023.Day21
         {
             long sum = 0;
 
-            int maxSteps = _useTestData ? 6 : 64;
+            int maxSteps = _useTestData ? 10 : 26501365;
             for (int i = 0; i < _data.Count; i++)
             {
                 bool isFound = false;
@@ -57,10 +57,10 @@ namespace AOC2023.Day21
 
         private void Bfs(int x, int y, int maxSteps)
         {
-            Queue<(int x, int y, int steps)> queue = new();
+            Queue<(int x, int y, bool didGoThroughWall, int steps)> queue = new();
             HashSet<(int x, int y)> seen = new();
 
-            queue.Enqueue((x, y, 0));
+            queue.Enqueue((x, y, false, 0));
             seen.Add((x, y));
 
             long visitCount = 0;
@@ -80,22 +80,33 @@ namespace AOC2023.Day21
                 foreach (var item in GetNeighbours(current.x, current.y))
                 {
                     _data[item.x][item.y] = 'O';
-                    queue.Enqueue((item.x, item.y, current.steps + 1));
+                    queue.Enqueue((item.x, item.y, item.didGoThroughWall, current.steps + 1));
                 }
 
-
-                var groups = queue.GroupBy(x => (x.x, x.y)).ToList();
+                //var groups = queue.GroupBy(x => (x.x, x.y)).ToList();
+                var groups = queue.GroupBy(x => (x.x, x.y, x.didGoThroughWall)).ToList();
                 queue.Clear();
-                foreach (var item in groups)
+                foreach (var group in groups)
                 {
-                    queue.Enqueue((item.First()));
+                    var first = group.First();
+                    if (first.didGoThroughWall)
+                    {
+                        //foreach (var tile in group)
+                        //{
+                        //    queue.Enqueue(tile);
+                        //}
+                    }
+                    else
+                    {
+                        queue.Enqueue(first);
+                    }
                 }
             }
 
             Console.WriteLine(visitCount);
         }
 
-        private List<(int x, int y)> GetNeighbours(int x, int y)
+        private List<(int x, int y, bool didGoThroughWall)> GetNeighbours(int x, int y)
         {
             List<(int x, int y)> dirs = new()
             {
@@ -103,18 +114,45 @@ namespace AOC2023.Day21
                 (-1, 0),
                 (0, 1),
                 (0, -1)
-            }, validDirs = new();
+            };
 
+            List<(int x, int y, bool didGoThroughWall)> validDirs = new();
             foreach (var item in dirs)
             {
+                bool didGoThroughWall = false;
                 var dx = item.x + x;
                 var dy = item.y + y;
-                if (dx < 0 || dx >= _data.Count || dy < 0 || dy >= _data[x].Count || _data[dx][dy] == '#')
+
+                if (dx < 0)
+                {
+                    dx = _data.Count - 1;
+                    didGoThroughWall = true;
+                }
+
+                if (dx >= _data.Count)
+                {
+                    dx = 0;
+                    didGoThroughWall = true;
+                }
+
+                if (dy < 0)
+                {
+                    dy = _data[x].Count - 1;
+                    didGoThroughWall = true;
+                }
+
+                if (dy >= _data[x].Count)
+                {
+                    dy = 0;
+                    didGoThroughWall = true;
+                }
+
+                if (_data[dx][dy] == '#')
                 {
                     continue;
                 }
 
-                validDirs.Add((dx, dy));
+                validDirs.Add((dx, dy, didGoThroughWall));
             }
 
             return validDirs;
