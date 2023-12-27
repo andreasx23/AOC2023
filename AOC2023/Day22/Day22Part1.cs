75b4f7462abaf483641de7c833e931ff58a75d53
ACA <andreas.cramer.antonsen@gmail.com>
2023-12-27 09:00:54 +0100

    Day 22 part 1 solved

--- AOC2023/Day22/Day22Part1.cs
+++ AOC2023/Day22/Day22Part1.cs
@@ -12,66 +12,55 @@ namespace AOC2023.Day22
 {
     public class Day22Part1
     {
-        class Brick
-        {
-            public char Id { get; set; }
-            public int LeftX { get; set; }
-            public int LeftY { get; set; }
-            public int LeftZ { get; set; }
-            public int RightX { get; set; }
-            public int RightY { get; set; }
-            public int RightZ { get; set; }
-        }
-
-        private static readonly bool _useTestData = true;
+        private static readonly bool _useTestData = false;
         private static readonly string _className = "Day22";
-        private List<Brick> _bricks = new();
+        private List<List<(int x, int y, int z)>> _bricks = new();
 
         public long Solve(Stopwatch watch)
         {
             long sum = 0;
 
-            // NOT SOLVED
-
-            var grid = CreateGrid();
+            HashSet<(int x, int y, int z)> seen = new();
+            foreach (var brick in _bricks)
+            {
+                foreach (var item in brick)
+                {
+                    seen.Add(item);
+                }
+            }
 
             while (true)
             {
-                bool isDone = true;
-                for (int z = grid[0][0].Count - 1; z > 0; z--)
+                var isDone = true;
+                for (int i = 0; i < _bricks.Count; i++)
                 {
-                    bool isValid = false;
-
-                    for (int x = 0; x < grid.Count; x++)
+                    List<(int x, int y, int z)> brick = _bricks[i];
+                    var ok = true;
+                    foreach (var item in brick)
                     {
-                        for (int y = 0; y < grid[x].Count; y++)
+                        if (item.z == 1 || seen.Contains((item.x, item.y, item.z - 1)) && !brick.Contains((item.x, item.y, item.z - 1)))
                         {
-                            isValid = IsFloorBelowUs(grid, x, y, z);
-
-                            if (isValid)
-                            {
-                                break;
-                            }
-                        }
-
-                        if (isValid)
-                        {
+                            ok = false;
                             break;
                         }
                     }
 
-                    if (!isValid)
+                    if (ok)
                     {
-                        for (int x = 0; x < grid.Count; x++)
+                        isDone = false;
+
+                        List<(int x, int y, int z)> newBrick = new();
+                        foreach (var item in brick)
                         {
-                            for (int y = 0; y < grid[x].Count; y++)
+                            if (!seen.Remove(item))
                             {
-                                grid[x][y][z] = ' ';
+                                throw new Exception("Invalid");
                             }
+                            seen.Add((item.x, item.y, item.z - 1));
+                            newBrick.Add((item.x, item.y, item.z - 1));
                         }
 
-                        isDone = false;
-                        break;
+                        _bricks[i] = newBrick;
                     }
                 }
 
@@ -81,84 +70,52 @@ namespace AOC2023.Day22
                 }
             }
 
-            //Print(grid);
-
-            return sum;
-        }
-
-        private bool IsFloorBelowUs(List<List<List<char>>> grid, int x, int y, int z)
-        {
-            List<(int x, int y, int z)> dirs = new(9)
+            for (int i = 0; i < _bricks.Count; i++)
             {
-                (0, 0, -1),
-
-                (0, 1, -1),
-                (0, -1, -1),
-                (1, 0, -1),
-                (-1, 0, -1),
-
-                (-1, -1, -1),
-                (-1, 1, -1),
-                (1, -1, -1),
-                (1, 1, -1),
-            };
-
-            foreach (var item in dirs)
-            {
-                var dx = item.x + x;
-                var dy = item.y + y;
-                var dz = item.z + z;
-
-                if (dx < 0 || dx >= grid.Count || dy < 0 || dy >= grid[dx].Count || dz < 0 || dz >= grid[dx][dy].Count)
+                List<(int x, int y, int z)> brick = _bricks[i];
+                foreach (var item in brick)
                 {
-                    continue;
+                    seen.Remove(item);
                 }
 
-                if (grid[dx][dy][dz] != ' ')
+                bool isValid = false;
+                for (int j = 0; j < _bricks.Count; j++)
                 {
-                    Console.WriteLine($"({dx}, {dy}, {dz}) {grid[dx][dy][dz]}");
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private List<List<List<char>>> CreateGrid()
-        {
-            var maxX = _bricks.Max(b => Math.Max(b.LeftX, b.RightX)) + 1;
-            var maxY = _bricks.Max(b => Math.Max(b.LeftY, b.RightY)) + 1;
-            var maxZ = _bricks.Max(b => Math.Max(b.LeftZ, b.RightZ)) + 1;
-
-            List<List<List<char>>> grid = new List<List<List<char>>>();
-            for (int i = 0; i < maxX; i++)
-            {
-                grid.Add(new List<List<char>>());
-                for (int j = 0; j < maxY; j++)
-                {
-                    grid[i].Add(new List<char>());
-                    for (int k = 0; k < maxZ; k++)
+                    if (i == j)
                     {
-                        grid[i][j].Add(' ');
+                        continue;
                     }
-                }
-            }
 
-            foreach (var brick in _bricks)
-            {
-                for (int x = brick.LeftX; x <= brick.RightX; x++)
-                {
-                    for (int y = brick.LeftY; y <= brick.RightY; y++)
+                    List<(int x, int y, int z)> b = _bricks[j];
+                    var ok = true;
+                    foreach (var item in b)
                     {
-                        for (int z = brick.LeftZ; z <= brick.RightZ; z++)
+                        if (item.z == 1 || seen.Contains((item.x, item.y, item.z - 1)) && !b.Contains((item.x, item.y, item.z - 1)))
                         {
-                            grid[x][y][z] = brick.Id;
+                            ok = false;
+                            break;
                         }
                     }
+
+                    if (ok)
+                    {
+                        isValid = true;
+                        break;
+                    }
+                }
+
+                if (!isValid) 
+                {
+                    sum++;
+                }
+
+                foreach (var item in brick)
+                {
+                    seen.Add(item);
                 }
             }
 
-            return grid;
+            return sum;
         }
 
         public void Result()
@@ -172,24 +129,42 @@ namespace AOC2023.Day22
         public void ReadData()
         {
             var lines = File.ReadAllLines(@$"{_className}\{(_useTestData ? "Test" : "Data")}.txt");
-            char id = 'A';
             foreach (var item in lines)
             {
                 var split = item.Split('~');
                 var left = split.First().Split(',').Select(int.Parse).ToList();
                 var right = split.Last().Split(',').Select(int.Parse).ToList();
-                _bricks.Add(new Brick()
-                {
-                    Id = id,
-                    LeftX = left[0],
-                    LeftY = left[1],
-                    LeftZ = left[2],
-                    RightX = right[0],
-                    RightY = right[1],
-                    RightZ = right[2],
-                });
 
-                id = (char)('A' + 1);
+                List<(int x, int y, int z)> brick = new List<(int x, int y, int z)>();
+                (int sx, int sy, int sz) = (left[0], left[1], left[2]);
+                (int ex, int ey, int ez) = (right[0], right[1], right[2]);
+                if (sx == ex && sy == ey)
+                {
+                    for (int z = sz; z < ez + 1; z++)
+                    {
+                        brick.Add((sx, sy, z));
+                    }
+                }
+                else if (sx == ex && sz == ez)
+                {
+                    for (int y = sy; y < ey + 1; y++)
+                    {
+                        brick.Add((sx, y, sz));
+                    }
+                }
+                else if (sy == ey && sz == ez)
+                {
+                    for (int x = sx; x < ex + 1; x++)
+                    {
+                        brick.Add((x, sy, sz));
+                    }
+                }
+                else
+                {
+                    throw new Exception("Invalid");
+                }
+
+                _bricks.Add(brick);
             }
         }
     }
